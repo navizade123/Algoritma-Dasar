2ac67d6b861f6bb7ccece6c535546b91ad743bab
navizade123 <122982228+navizade123@users.noreply.github.com>
2023-11-07 15:02:24 +0700

    Add files via upload

---
+++ XII RPL_Naviz/script.js
@@ -0,0 +1,13 @@
+let Hujan = confirm("Apakah Hujan ?")
+
+
+if (Hujan == true) {
+    let Payung = confirm("Silahkan cari payung")
+    if (Payung == true){
+        alert ("bisa keluar");
+    } else {
+        alert ("ga bisa keluar")
+    }
+} else {
+    alert("bisa keluar")
+}
